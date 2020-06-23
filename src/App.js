788db25047fd8088acd7176e130e114320fd086b
Liz Shulman <corgipower@hotmail.com>
2020-06-23 19:30:07 -0400

    header styled

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="App-logo">
+        <p>
+          NASA Photo of the Day
+        </p>
+      </div>
     </div>
   );
 }
